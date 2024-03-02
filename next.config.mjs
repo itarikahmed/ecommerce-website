2e07f6d2da3e105a414953cf3c934f93854562aa
itarikahmed <tarik.ahmed@northsouth.edu>
2024-03-03 02:21:12 +0600

    added packages and pictures

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: "http://localhost:3000",
+    DB_URI: "mongodb://localhost:27017/buyitnow",
+
+    CLOUD_NAME: "",
+    CLOUDINARY_API_KEY: "",
+    CLOUDINARY_API_SECRET: "",
+
+    STRIPE_PUBLIC_KEY: "",
+    STRIPE_PRIVATE_KEY: "",
+
+    STRIPE_WEBHOOK_SECRET: "",
+
+    NEXTAUTH_SECRET: "codingwithabbas",
+  },
+};
 
 export default nextConfig;
